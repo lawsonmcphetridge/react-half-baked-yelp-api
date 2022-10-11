6cb95e982812053340159c24e1ed10efc753d23b
Lawson <lawsonmcphetridge2020@gmail.com>
2022-10-11 12:20:17 -0700

    added search functinality by zip and term

--- functions/fetch-yelp/index.js
+++ functions/fetch-yelp/index.js
@@ -2,6 +2,29 @@ const fetch = require('node-fetch');
 require('dotenv').config({ path: `.env.development.local` });
 
 const handler = async (event) => {
+  const zip = event.queryStringParameters.zip;
+  const search = event.queryStringParameters.search;
+  try {
+    const resp = await fetch(
+      `https://api.yelp.com/v3/businesses/search?categories=restaurants&location=${zip}&term=${search}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`
+        },
+      }
+    );
+    const data = await resp.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.businesses)
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed Fetching Data' })
+    };
+  }
   // add code here to fetch data from yelp API
   // be sure to include the parameters from event.queryStringParameters
 };
